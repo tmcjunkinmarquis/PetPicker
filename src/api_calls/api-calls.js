4095cfb7c10f863b3a229441bfdbba6d684113cc
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-13 17:04:27 -0600

    Render first pet from fetch

--- src/api_calls/api-calls.js
+++ src/api_calls/api-calls.js
@@ -4,4 +4,17 @@ const fetchUsers = ()=>{
   console.log('I will fetch users')
 }
 
-export { fetchUsers };
+const fetchPets = ()=>{
+  const url = 'https://pet-picker-api.herokuapp.com/api/v1/users/1/pets'
+  
+  return fetch(url)
+  .then((response)=>{
+    return response.json()
+  })
+  .then((pets)=>{
+    return pets
+  })
+  
+}
+
+export { fetchUsers, fetchPets };
