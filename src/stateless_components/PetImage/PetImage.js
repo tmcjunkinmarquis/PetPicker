d0ddf396b9a716e354d4e3ced277bf671c9ef5be
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-14 09:31:26 -0600

    Add snapshot test for PetImage

--- src/stateless_components/PetImage/PetImage.js
+++ src/stateless_components/PetImage/PetImage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import './PetImage.css'
+import './PetImage.css';
+import PropTypes from 'prop-types';
 
 const PetImage = (props) => {
 
   const petUrl = props.pet.pic;
   return (
     <img src={petUrl}></img>
-  )
-}
+  );
+};
 
 export default PetImage;
+
+PetImage.propTypes = {
+  name: PropTypes.string,
+  pic: PropTypes.string
+};
--- src/stateless_components/PetsContainer/PetsContainer.js
+++ src/stateless_components/PetsContainer/PetsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import './PetsContainer.css'
-import PetImage from '../PetImage/PetImage'
+import PetImage from '../PetImage/PetImage';
 
 const PetsContainer = ({ petsArray, loggedIn }) => {
 
@@ -8,28 +8,28 @@ const PetsContainer = ({ petsArray, loggedIn }) => {
   const makePets = (petsArray) => {
     if (petsArray.length) {
 
-      let petZero = petsArray[0]
+      let petZero = petsArray[0];
 
       if (!loggedIn) {
         return (
           <PetImage pet={petZero} />
-        )
+        );
       } else {
         return petsArray.map((pet) => {
           return (
             <PetImage pet={pet} />
-          )
-        })
+          );
+        });
       }
     }
-  }
+  };
 
   return (
     <div className="pets-container">
       {makePets(petsArray)}
     </div>
-  )
+  );
 
-}
+};
 
 export default PetsContainer;
