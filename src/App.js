14ba0bd5f2efdfd367f0a33d52a0ce1bae7c406d
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-15 10:55:17 -0600

    Include matches route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import Logo from '../src/components/Logo/Logo';
 import './App.css';
 import { Route, NavLink } from 'react-router-dom';
 import Pets from './components/Pets/Pets'
 import Profile from "./components/Profile/Profile";
 import Login from "./components/Login/Login";
+import NavigationBar from '../src/components/NavigationBar/NavigationBar';
+import Matches from '../src/components/Matches/Matches';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavigationBar />
         <Route path='/' component={Pets} />
         <Route path='/profile' component={Profile} />
-        {/* <Route path='/matches' component={Matches} /> */}
+        <Route path='/matches' component={Matches} />
         <Route path='/pets' component={Pets} />
         <Route path='/login' component={Login} />
       </div >
