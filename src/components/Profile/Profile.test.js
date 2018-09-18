0f2ca72ff2cb5c6ffc251867c336b9583e010b74
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-18 15:06:31 -0600

    Welcome test

---
+++ src/components/Profile/Profile.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable max-len */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Profile } from './Profile';
+// import { fetchWelcomePet } from '../../api_calls/api-calls';
+// jest.mock('../../api_calls/api-calls.js');
+
+describe('Profile', () => {
+  let wrapper;
+  let mockProps;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockProps = {
+      userId: '1',
+      loggedIn: false,
+      history: { push: jest.fn() }
+    };
+    wrapper = shallow(<Profile {...mockProps} />, {
+      disableLifecycleMethods: true
+    });
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleChange', () => {
+    it('sets state with event target name & value', () => {
+      const mockEvent = { target: { name: 'status', value: 'adopter' } };
+      const expected = { status: 'adopter' };
+
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state()).toEqual(expect.objectContaining(expected));
+    });
+  });
+
+  describe('componentDidMount', () => {
+    it('calls history.push', () => {
+      wrapper.instance().componentDidMount();
+
+      expect(mockProps.history.push).toHaveBeenCalledTimes(1);
+    });
+  });
+});
