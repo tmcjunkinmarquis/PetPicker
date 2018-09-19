1d82d5b1a4c79d9175e4874ae79af1e9d1287b57
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-18 20:57:45 -0600

    Pass like test

--- src/components/LikeButton/LikeButton.test.js
+++ src/components/LikeButton/LikeButton.test.js
@@ -2,7 +2,10 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LikeButton } from './LikeButton';
+import { LikeButton, mapStateToProps, mapDispatchToProps } from './LikeButton';
+import * as actions from '../../actions';
+import { fetchLikePostPet } from '../../api_calls/api-calls';
+jest.mock('../../api_calls/api-calls.js');
 
 describe('LikeButton', () => {
   let wrapper;
@@ -10,7 +13,12 @@ describe('LikeButton', () => {
 
   beforeEach(() => {
     jest.resetAllMocks();
-    mockProps = {};
+    mockProps = {
+      petsArray: [{}, {}],
+      userId: 1,
+      storeAllPets: jest.fn(),
+      updatePets: jest.fn()
+    };
     wrapper = shallow(<LikeButton {...mockProps} />, {
       disableLifecycleMethods: true
     });
@@ -18,5 +26,58 @@ describe('LikeButton', () => {
 
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleLikeClick', () => {
+    it('calls fetchLikePostPet', async () => {
+      await wrapper.instance().handleLikeClick();
+
+      expect(fetchLikePostPet).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls updatePets', async () => {
+      await wrapper.instance().handleLikeClick();
+
+      expect(mockProps.updatePets).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('maps state properties to props', () => {
+      const mockState = {
+        user: {
+          id: '3242'
+        },
+        pets: {
+          petsArray: []
+        }
+      };
+      const expected = {
+        userId: '3242',
+        petsArray: []
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with the correct params on storeAllPets', () => {
+      const petsArray = [{}];
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      const mockAction = actions.petsArray(petsArray);
+      mappedProps.storeAllPets(petsArray);
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+    });
+
+    it('calls dispatch with the correct params on updatePets', () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      const mockAction = actions.updatePets();
+      mappedProps.updatePets();
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+    });
   });
 });
