674cefc105fd451e253fa84734a7bfcb008ba4d0
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-18 23:08:59 -0600

    Pass Indiv match tests

--- src/components/IndividualMatch/IndividualMatch.test.js
+++ src/components/IndividualMatch/IndividualMatch.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable max-len */
 
-import React from 'react';
-import { shallow } from 'enzyme';
-import { IndividualMatch } from './IndividualMatch';
+import React from "react";
+import { shallow } from "enzyme";
+import { IndividualMatch } from "./IndividualMatch";
+import { fetchPostMakeMatch } from "../../api_calls/api-calls";
+jest.mock("../../api_calls/api-calls.js");
 
-describe('IndividualMatch', () => {
+describe("IndividualMatch", () => {
   let wrapper;
   let mockProps;
 
@@ -14,7 +16,10 @@ describe('IndividualMatch', () => {
       match: {
         id: 10,
         name: "Ralph",
-        description: "Doggo ipsum heckin angery woofer wow such tempt fl…treat waggy wags he made many woofs noodle horse.", pic: "https://static1.squarespace.com/static/53a096cce4b…6922235/Shake+Puppies-1_Mixbreed.jpg?format=1500w"
+        description:
+          "Doggo ipsum heckin angery woofer wow such tempt fl…treat waggy wags he made many woofs noodle horse.",
+        pic:
+          "https://static1.squarespace.com/static/53a096cce4b…6922235/Shake+Puppies-1_Mixbreed.jpg?format=1500w"
       },
       user: 1
     };
@@ -23,7 +28,16 @@ describe('IndividualMatch', () => {
     });
   });
 
-  it('matches the snapshot', () => {
+  it("matches the snapshot when match is an not an owner", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("matches the snapshot when match is an owner", () => {
+    mockProps.match.owner = true;
+    wrapper = shallow(<IndividualMatch {...mockProps} />, {
+      disableLifecycleMethods: true
+    });
+
     expect(wrapper).toMatchSnapshot();
   });
 });
